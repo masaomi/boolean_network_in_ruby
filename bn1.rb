9aa92dbbd5f2c744dc5f1e8abf7abc642b1cb559
masaomi <masaomi.hatakeyama@gmail.com>
2020-11-25 06:14:09 +0100

    Added bn1.rb, done

--- bn1.rb
+++ bn1.rb
@@ -1,30 +1,47 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
-# Version = '20201124-063942'
+# Version = '20201125-061006'
 
 # bit operator
 # & | ^ ~
 
-
+# node list
+# initial states
 nodes = [0,0,0]
 
 # link and operations matrix
+# from colum to row, this is simple
 # & only
 # -1: ~, 
-m = [ [-1, 1, 0],
-      [0,  0, 1],
-      [0,  0, 0]
+m = [ [-1, 0, 0],
+      [1,  0, 0],
+      [0,  1, 0]
 ]
-
-# p m
-# exit
+# example
+# one_step =->() do
+#   pnodes = nodes.clone
+#   nodes[0] = (~pnodes[0]) & 1
+#   nodes[1] = pnodes[0] & 1
+#   nodes[2] = pnodes[1] & 1
+# end
 
 one_step =->() do
   pnodes = nodes.clone
-  nodes[0] = (~pnodes[0]) & 1
-  nodes[1] = pnodes[0] & 1
-  nodes[2] = pnodes[1] & 1
+  m.each_with_index do |row, i|
+    nodes[i] = row.each_with_index.inject(1) do |s, pair|
+      element, index = pair
+      case element
+      when 1
+        s &= pnodes[index]
+      when -1
+        s &= ~pnodes[index]
+      end
+      s
+    end
+  end
 end
+
+
 print_headers =->() do
   puts "step|nodes"
   puts "-"*4 + "+" + "-"*20
